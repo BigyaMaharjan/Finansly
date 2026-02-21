d7c186c82bf7903a9589861c486a7fa86d03fd78
BigyaMaharjan <93863599+BigyaMaharjan@users.noreply.github.com>
2026-02-22 01:16:15 +0545

    feat: ✨ adds openApi to swagger UI

--- Finansly.Presentation/Program.cs
+++ Finansly.Presentation/Program.cs
@@ -47,7 +47,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
