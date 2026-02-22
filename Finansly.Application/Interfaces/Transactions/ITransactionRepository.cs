be07de65d96c2754e29ca7768052f33c984efd7d
BigyaMaharjan <93863599+BigyaMaharjan@users.noreply.github.com>
2026-02-23 01:12:06 +0545

    feat: ✨ adds transaction Interface

---
+++ Finansly.Application/Interfaces/Transactions/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Finansly.Application.Common.Interfaces;
+using Finansly.Domain.Entities;
+using Finansly.Domain.Enums;
+
+namespace Finansly.Application.Interfaces.Transactions;
+
+public interface ITransactionRepository : IBaseRepository<Transaction>
+{
+    Task<IEnumerable<Transaction>> GetByUserAsync(int userId);
+    Task<decimal> GetTotalByTypeAsync(int userId, CategoryType type, int month, int year);
+}
