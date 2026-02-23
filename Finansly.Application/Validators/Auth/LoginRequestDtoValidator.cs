2dc95ac49a568782aecfec0ea6dccccc3cc6f9ff
BigyaMaharjan <93863599+BigyaMaharjan@users.noreply.github.com>
2026-02-24 00:25:10 +0545

    feat: ✨ adds loginRequestDto validator

---
+++ Finansly.Application/Validators/Auth/LoginRequestDtoValidator.cs
@@ -0,0 +1,18 @@
+using Finansly.Application.DTOs.Auth;
+using FluentValidation;
+
+namespace Finansly.Application.Validators.Auth;
+
+public class LoginRequestDtoValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Email is not valid.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters.");
+    }
+}
