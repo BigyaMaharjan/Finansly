e95311659e60d2844a82d67c7ebefe5493ac9b00
BigyaMaharjan <93863599+BigyaMaharjan@users.noreply.github.com>
2026-02-24 00:36:57 +0545

    feat: ✨ adds RegisterRequestDto to AuthDto

--- Finansly.Application/DTOs/Auth/AuthDtos.cs
+++ Finansly.Application/DTOs/Auth/AuthDtos.cs
@@ -6,6 +6,14 @@ public record LoginRequestDto
     public string Password { get; init; } = null!;
 }
 
+public record RegisterRequestDto
+{
+    public string Name { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public DateTime DateOfBirth { get; init; }
+}
+
 public record AuthResponseDto
 {
     public string Token { get; init; } = null!;
