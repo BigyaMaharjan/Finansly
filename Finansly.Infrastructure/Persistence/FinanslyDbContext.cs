307a0b7ed606fa5d6154cd901ba3d4ca62dbc6c3
BigyaMaharjan <93863599+BigyaMaharjan@users.noreply.github.com>
2026-02-25 08:49:48 +0545

    fix: 🐛 implements maxlength in model building

--- Finansly.Infrastructure/Persistence/FinanslyDbContext.cs
+++ Finansly.Infrastructure/Persistence/FinanslyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Finansly.Domain.Entities;
+﻿using Finansly.Domain.Constants;
+using Finansly.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Finansly.Infrastructure.Persistence;
@@ -15,6 +16,14 @@ public class FinanslyDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        // Max lengths
+        modelBuilder.Entity<User>().Property(u => u.Name).HasMaxLength(EntityLengths.User.Name);
+        modelBuilder.Entity<User>().Property(u => u.Email).HasMaxLength(EntityLengths.User.Email);
+        modelBuilder.Entity<User>().Property(u => u.PasswordHash).HasMaxLength(EntityLengths.User.PasswordHash);
+        modelBuilder.Entity<User>().Property(u => u.Bio).HasMaxLength(EntityLengths.User.Bio);
+        modelBuilder.Entity<Category>().Property(c => c.Name).HasMaxLength(EntityLengths.Category.Name);
+        modelBuilder.Entity<Transaction>().Property(t => t.Description).HasMaxLength(EntityLengths.Transaction.Description);
 
         // Unique email
         modelBuilder.Entity<User>()
