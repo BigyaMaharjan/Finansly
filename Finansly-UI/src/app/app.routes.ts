6dc47a378e30545269aec3cae1fe2a3fc4af2366
BigyaMaharjan <93863599+BigyaMaharjan@users.noreply.github.com>
2026-03-20 11:45:07 +0545

    feat: :lipstick: adds app components

--- Finansly-UI/src/app/app.routes.ts
+++ Finansly-UI/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+];
