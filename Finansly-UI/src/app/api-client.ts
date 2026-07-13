bc7cf9993372aca308c2937dc25782efaa27d815
Bigya Maharjan <bigyashadow@gmail.com>
2026-07-13 15:10:01 +0545

    feat: :lipstick: adds image and animation to register

--- Finansly-UI/src/app/api-client.ts
+++ Finansly-UI/src/app/api-client.ts
@@ -173,7 +173,7 @@ export class Client implements IClient {
     @Optional() @Inject(API_BASE_URL) baseUrl?: string,
   ) {
     this.http = http;
-    this.baseUrl = baseUrl ?? 'http://localhost:8080/';
+    this.baseUrl = baseUrl ?? 'https://localhost:7007';
   }
 
   /**
