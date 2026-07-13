f8e7b139f905af69f7ac6cdf78119506c5ff9fa9
Bigya Maharjan <bigyashadow@gmail.com>
2026-07-13 17:05:11 +0545

    feat: :lipstick: Adds Category page

--- Finansly-UI/src/app/app.routes.ts
+++ Finansly-UI/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { Category } from './components/category/category';
 
 export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
+  { path: 'categories', component: Category },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
 ];
