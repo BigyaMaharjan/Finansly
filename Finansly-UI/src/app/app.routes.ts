f2a0c9309093059a62ab76e71a9113c8fa9de0ec
Bigya Maharjan <bigyashadow@gmail.com>
2026-07-14 16:28:51 +0545

    feat: :sparkles: adds app routes for auth sessions

--- Finansly-UI/src/app/app.routes.ts
+++ Finansly-UI/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { Category } from './components/category/category';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
-  { path: 'categories', component: Category },
+  { path: 'categories', component: Category, canActivate: [authGuard] },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
 ];
