037d5c59bc21e1fab0d175675ac26eb436ec092f
Bigya Maharjan <bigyashadow@gmail.com>
2026-07-15 14:50:20 +0545

    feat: :lipstick: styles the nav bar

--- Finansly-UI/src/app/app.ts
+++ Finansly-UI/src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, computed, inject, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SessionTimeoutComponent } from './components/session-timeout/session-timeout.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,13 @@ export class App {
   protected readonly title = signal('finansly-ui');
 
   private router = inject(Router);
+  private currentUrl = signal(this.router.url);
 
-  showNavbar = computed(() => {
-    const url = this.router.url;
-    return !url.startsWith('/auth');
-  });
+  showNavbar = computed(() => !this.currentUrl().startsWith('/auth'));
+
+  constructor() {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e) => this.currentUrl.set(e.urlAfterRedirects));
+  }
 }
